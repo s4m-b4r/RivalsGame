425cebb03e1d05865d2e426e7a047363db19bb65
s4m-b4r <samuel.barker2987@gmail.com>
2025-10-09 10:48:35 +0100

    preload

--- public/script.js
+++ public/script.js
@@ -18,15 +18,6 @@ socket.on("bullet_shot", (data) => {
 	console.log("bullet_shot", data);
 });
 
-function preload() {
-	// Load any assets here if needed
-	assaultRifleImage = loadImage("assets/Guns/AK47.png");
-	tileset = loadImage("assets/environment/tileset.png");
-	rifleAmmo = loadImage("assets/Bullets/RifleAmmoSmall.png");
-	rifleShot = loadSound("assets/Sounds/762x54r Single Isolated MP3.mp3");
-	rifleReload = loadSound("assets/Sounds/ak-47-reload-sound-effect.wav");
-}
-
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	rectMode(CORNER);
