3310a91ba0ba2cc8d670ce324de5dd343d712f96
s4m-b4r <samuel.barker2987@gmail.com>
2025-10-23 10:43:48 +0100

    x2

--- public/particles.js
+++ public/particles.js
@@ -18,8 +18,8 @@ class Particle {
 		push();
 		translate(this.x, this.y);
 
-		rotate(this.rotation.heading() + 90);
-		console.log(this.rotation.heading());
+		rotate(this.rotation.heading() + 0.5 * Math.PI);
+
 		noSmooth();
 		image(this.asset, 0, 0, 64, 64, this.frameCount * 64, 0, 64, 64);
 		pop();
