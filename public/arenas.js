0302457c8801a9ebafdb9c0c4607de6c6488a70a
s4m-b4r <samuel.barker2987@gmail.com>
2025-10-24 14:41:31 +0100

    test2

--- public/arenas.js
+++ public/arenas.js
@@ -79,29 +79,17 @@ function arenaAssetsLoad() {
 					arenaTiles[i][j] = { x: 3, y: 2 }; // inbetween vertical
 				}
 			} else {
-				if (!up && !down && !left && !right) {
-					arenaTiles[i][j] = { x: 3, y: 1 }; // middle
-				} else if (!up && down && !left && !right) {
-					arenaTiles[i][j] = { x: 1, y: 5 }; // bottom middle
-				} else if (!up && down && left && !right) {
-					arenaTiles[i][j] = { x: 1, y: 2 }; // bottom left
-				} else if (!up && down && !left && right) {
-					arenaTiles[i][j] = { x: 2, y: 2 }; // bottom right
-				} else if (!up && !down && left && !right) {
-					arenaTiles[i][j] = { x: 2, y: 4 }; // left side
-				} else if (!up && !down && !left && right) {
-					arenaTiles[i][j] = { x: 4, y: 1 }; // right side
-				} else if (up && !down && !left && !right) {
-					arenaTiles[i][j] = { x: 3, y: 2 }; // top middle
-				} else if (up && !down && left && !right) {
-					arenaTiles[i][j] = { x: 1, y: 1 }; // top left
-				} else if (up && !down && !left && right) {
-					arenaTiles[i][j] = { x: 2, y: 1 }; // top right
-				} else if (up && down && !left && !right) {
-					arenaTiles[i][j] = { x: 3, y: 3 }; // inbetween horizontal
-				} else if (!up && !down && left && right) {
-					arenaTiles[i][j] = { x: 2, y: 3 }; // inbetween vertical
-				}
+				if (!up && !down && !left && !right) arenaTiles[i][j] = { x: 3, y: 1 };
+				else if (!up && down && !left && !right) arenaTiles[i][j] = { x: 2, y: 5 };
+				else if (up && !down && !left && !right) arenaTiles[i][j] = { x: 1, y: 5 };
+				else if (!up && !down && !left && right) arenaTiles[i][j] = { x: 1, y: 4 };
+				else if (!up && !down && left && !right) arenaTiles[i][j] = { x: 2, y: 4 };
+				else if (up && down && !left && !right) arenaTiles[i][j] = { x: 1, y: 3 };
+				else if (!up && !down && left && right) arenaTiles[i][j] = { x: 2, y: 3 };
+				else if (up && !down && !left && right) arenaTiles[i][j] = { x: 1, y: 2 };
+				else if (up && !down && left && !right) arenaTiles[i][j] = { x: 2, y: 2 };
+				else if (!up && down && !left && right) arenaTiles[i][j] = { x: 1, y: 1 };
+				else if (!up && down && left && !right) arenaTiles[i][j] = { x: 2, y: 1 };
 			}
 		}
 	}
